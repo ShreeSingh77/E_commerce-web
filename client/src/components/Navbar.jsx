b8fab0fc1c13a9607000ee2c4f3b358dfc69d15a
Shrees Singh <sshridhanshi@gmail.com>
2026-07-22 13:26:56 +0530

    feat: Enhance Navbar with icon links and improved layout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,51 @@
 import { NavLink } from "react-router-dom";
+import { FiSearch, FiHeart, FiShoppingCart, FiUser } from "react-icons/fi";
 import "./Navbar.css";
 
 const Navbar = () => {
+
+  const navLinks = [
+    { name: "Home", path: "/" },
+    { name: "Products", path: "/products" },
+  ];
+
   return (
     <nav className="navbar">
-      <h2 className="logo">E-Commerce</h2>
+
+      <NavLink to="/" className="logo">
+        E-Commerce
+      </NavLink>
 
       <div className="nav-links">
-        <NavLink to="/">Home</NavLink>
 
-        <NavLink to="/products">Products</NavLink>
+        {navLinks.map((link) => (
+          <NavLink key={link.path} to={link.path}>
+            {link.name}
+          </NavLink>
+        ))}
 
-        <NavLink to="/cart">Cart</NavLink>
-
-        <NavLink to="/wishlist">Wishlist</NavLink>
-
-        <NavLink to="/login">Login</NavLink>
       </div>
+
+      <div className="nav-icons">
+
+        <NavLink to="/wishlist">
+          <FiHeart />
+        </NavLink>
+
+        <NavLink to="/cart">
+          <FiShoppingCart />
+        </NavLink>
+
+        <button className="search-btn">
+          <FiSearch />
+        </button>
+
+        <NavLink to="/login">
+          <FiUser />
+        </NavLink>
+
+      </div>
+
     </nav>
   );
 };
